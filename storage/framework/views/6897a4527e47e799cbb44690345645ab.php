fa13676e56aac65ed44ef8f0dfd3aab0a8901a9b
Muhammad Alif Satriadhi <137528720+S4triadhi@users.noreply.github.com>
2024-09-08 14:05:14 +0700

    Delete storage directory

--- storage/framework/views/6897a4527e47e799cbb44690345645ab.php
+++
@@ -1,7 +0,0 @@
-<h1>Prima Check for n = <?php echo e($n); ?></h1>
-<ul>
-    <?php $__currentLoopData = $numbers; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $number): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <li><?php echo e($number['number']); ?> is <?php echo e($number['is_prime'] ? 'Prima' : 'Bukan Prima'); ?></li>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</ul>
-<?php /**PATH D:\# ITS\# Matkul\Semester 5\PBKK\Tugas 1\tugas1\resources\views/number/prima.blade.php ENDPATH**/ ?>
