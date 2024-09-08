cf2bd20339a313a1f8ddaf2e58e85a9fc840c22d
Muhammad Alif Satriadhi <137528720+S4triadhi@users.noreply.github.com>
2024-09-08 14:04:13 +0700

    Delete resources directory

--- resources/views/routes/named.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Named Routes')
-
-@section('page-title', 'Named Routes')
-
-@section('content')
-<p>Ini adalah contoh Named Routes di Laravel.</p>
-@endsection
