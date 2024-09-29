744a3adc0e51530f05bb47216746c0ea766da235
Muhammad Alif Satriadhi <137528720+S4triadhi@users.noreply.github.com>
2024-09-29 16:27:48 +0700

    Delete resources directory

--- resources/views/routes/basic.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Basic Routing')
-
-@section('page-title', 'Basic Routing')
-
-@section('content')
-<p>Ini adalah contoh Basic Routing di Laravel.</p>
-@endsection
