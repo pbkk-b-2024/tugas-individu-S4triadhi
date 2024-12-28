0b371b4f0b482b83968043bebe6f121153d7a85a
Muhammad Alif Satriadhi <137528720+S4triadhi@users.noreply.github.com>
2024-12-28 20:25:46 +0700

    Delete resources directory

--- resources/views/number/prima.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Prima Check for n = {{ $n }}</h1>
-<ul>
-    @foreach ($numbers as $number)
-        <li>{{ $number['number'] }} is {{ $number['is_prime'] ? 'Prima' : 'Bukan Prima' }}</li>
-    @endforeach
-</ul>
