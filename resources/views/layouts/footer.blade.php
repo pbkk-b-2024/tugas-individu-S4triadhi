3826bb633ce1c2f7a5c5655b03f3a39ca0ed9684
Muhammad Alif Satriadhi <137528720+S4triadhi@users.noreply.github.com>
2024-12-28 20:20:06 +0700

    Delete resources directory

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="#">Your Company</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.5
-    </div>
-</footer>
